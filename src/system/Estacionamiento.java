bdb5a936585bca0674ac99d8c23d03464f89f554
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2021-10-23 23:38:33 -0300

    agregando la clase Estacionamiento

---
+++ src/system/Estacionamiento.java
@@ -0,0 +1,11 @@
+package system;
+
+import java.time.LocalTime;
+
+public class Estacionamiento {
+	private LocalTime horaDeInicio;
+	private LocalTime horaDeFinalizacion;
+	private String patente;
+	
+	
+}
