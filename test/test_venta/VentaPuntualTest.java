110886c32fb0b64ca32956df35ca1a8a758cdd87
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2021-10-30 01:43:55 -0300

    add: setup para test de Ventas

---
+++ test/test_venta/VentaPuntualTest.java
@@ -0,0 +1,14 @@
+package test_venta;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VentaPuntualTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/test_venta/VentaRecargaTest.java
@@ -0,0 +1,52 @@
+package test_venta;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sem_PuntoDeVenta.PuntoDeVenta;
+import sem_venta.VentaPuntual;
+import sem_venta.VentaRecarga;
+
+class VentaRecargaTest {
+	
+	PuntoDeVenta pv; 
+	
+	int idPV;
+	int coordenadaPV;
+	
+	LocalTime hora;
+	LocalTime fecha;
+	
+	//Atributos de ventaRecarga
+	int celular;
+	int monto;
+	
+	//Atributos de ventaPuntual
+	String patente;
+	int horasCompradas;
+	
+	VentaPuntual vPuntual;
+	VentaRecarga vRecarga;
+	
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		idPV = 123455;
+		coordenadaPV = 60552;
+		
+		hora = LocalTime.of(LocalTime.now().getHour(),0);
+		fecha = LocalTime.now().get;
+		
+	}
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
