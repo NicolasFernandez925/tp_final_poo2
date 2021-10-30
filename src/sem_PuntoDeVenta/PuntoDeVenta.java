1b46c6b2ab16f0e5413900fe300c7e4c1e8019cd
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2021-10-30 16:10:03 -0300

    feat: colaborador Numerador y asignarNroControl

--- src/sem_PuntoDeVenta/PuntoDeVenta.java
+++ src/sem_PuntoDeVenta/PuntoDeVenta.java
@@ -2,6 +2,7 @@ package sem_PuntoDeVenta;
 
 import java.util.ArrayList;
 
+import sem_venta.Numerador;
 import sem_venta.Venta;
 import sem_venta.VentaPuntual;
 import sem_venta.VentaRecarga;
@@ -9,15 +10,14 @@ import sem_venta.VentaRecarga;
 public class PuntoDeVenta implements ISEMPuntoDeVenta{
 	private int coordenada;
 	private int id;
-	// Aca podria ser una lista de tipo venta, en vez que haya 2 arrays tanto para recargas
-	// como para ventas puntuales. Los metodos 
+	private Numerador numerador;
 	private ArrayList<Venta> ventas; 
-	//private ArrayList<VentaRecarga> recargas; 
-	//private ArrayList<VentaPuntual> ventasPuntuales; 
+
 	
 	public PuntoDeVenta(int id, int coordenada) {
 		this.id = id;
 		this.coordenada = coordenada;
+		this.numerador = new Numerador();
 	}
 
 	@Override
@@ -28,8 +28,8 @@ public class PuntoDeVenta implements ISEMPuntoDeVenta{
 
 	@Override
 	public void hacerRecarga(int nroCelular, int monto) {
-		// TODO Auto-generated method stub
-		
+		VentaRecarga venta = new VentaRecarga(nroCelular, monto, this, this.asignarSiguienteNroDeControl());
+		this.ventas.add(venta);
 	}
 
 	@Override
@@ -41,19 +41,9 @@ public class PuntoDeVenta implements ISEMPuntoDeVenta{
 	public int getCoordenada() {
 		return this.coordenada;
 	}
-	
-	// Estos metodos  podrian quitarse ya que fueron opcionales, no los pide el TP
-	// ademas que eso nos complica para calcularlo ya que se plante solo tener un array de Ventas y no 
-	// de c/u
-	/*public int totalDeHorasVendidas() {
-	
-		return ventasPuntuales.stream().mapToInt(VentaPuntual::getCantidadDeHoras).sum(); 
+
+	public int asignarSiguienteNroDeControl() {
+		return numerador.siguienteNroDeControl();
 	}
-	
-	public double totalMontoDeRecargas() {
-		return 1;
-	}
-	*/
-	
 
 }
